9334cb61aa7d3b7623e3705d550edab4b9396684
abhishekbahukhandi <abhishekbahukhandi2020@gmail.com>
2025-05-12 16:10:58 +0530

    cors fix

--- app.js
+++ app.js
@@ -40,7 +40,7 @@
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',  // Replace with your frontend URL
+  origin:[ 'http://localhost:5173','https://ragchatbotbackend.vercel.app'],  // Replace with your frontend URL
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
